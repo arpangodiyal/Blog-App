feef0634cc8fb68c7cfd4f7f00d06d9d6c3257ce
Arpan Godiyal <arpangodiyal24@gmail.com>
2019-03-20 13:52:26 +0530

    Feeds and Sitemap

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostsFeed
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:year>/<int:month>/<int:day>/<slug:title>', views.detail, name='detail'),
-    path('share/<int:postId>', views.sharePost, name='sharePost')
-    #path('<slug:title>', views.detail, name='detail')
+    path('share/<int:postId>', views.sharePost, name='sharePost'),
+    path('feeds', LatestPostsFeed(), name='feeds')
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'blog'
 ]
 
@@ -125,3 +127,5 @@ EMAIL_HOST_USER = 'servercall200@gmail.com'
 EMAIL_HOST_PASSWORD = 'hellobrother100'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+SITE_ID=4
